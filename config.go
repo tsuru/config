3fcef36a67ef4e33f8825fd9637856dae67c97b1
Francisco Souza <f@souza.cc>
2012-10-03 21:57:18 -0300

    license: added license information (BSD-3)

--- config.go
+++ config.go
@@ -1,3 +1,7 @@
+// Copyright 2012 config authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package config provide configuration facilities, handling configuration
 // files in yaml format.
 package config
