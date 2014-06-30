f4d6d39615a37c66a013448cda92c2d3f889bdec
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2014-06-30 16:49:11 -0300

    make checker abstract. It receives a slice of Checkers and executes them.

--- checker.go
+++ checker.go
@@ -4,93 +4,14 @@
 
 package config
 
-import (
-	"errors"
-	"fmt"
-)
+type Checker func() error
 
 // Check a parsed config file.
-func Check() error {
-	return CheckProvisioner()
-
-}
-
-// Check provisioner configs
-func CheckProvisioner() error {
-	if configs["provisioner"] == "docker" {
-		return CheckDocker()
-	}
-	return nil
-}
-
-// Check Docker configs
-func CheckDocker() error {
-	if _, err := Get("docker"); err != nil {
-		return errors.New("Config Error: you should configure docker.")
-	}
-	err := CheckDockerBasicConfig()
-	if err != nil {
-		return err
-	}
-	err = CheckScheduler()
-	if err != nil {
-		return err
-	}
-	err = CheckRouter()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Check default configs to Docker.
-func CheckDockerBasicConfig() error {
-	basicConfigs := []string{
-		"docker:repository-namespace",
-		"docker:collection",
-		"docker:deploy-cmd",
-		"docker:ssh-agent-port",
-		"docker:ssh",
-		"docker:ssh:add-key-cmd",
-		"docker:ssh:public-key",
-		"docker:ssh:user",
-		"docker:run-cmd:bin",
-		"docker:run-cmd:port",
-	}
-	for _, key := range basicConfigs {
-		if _, err := Get(key); err != nil {
-			return fmt.Errorf("Config Error: you should configure %s", key)
-		}
-	}
-	return nil
-}
-
-// Check Schedulers
-// It's verify your scheduler configuration and validate related confs.
-func CheckScheduler() error {
-	if scheduler, err := Get("docker:segregate"); err == nil && scheduler == true {
-		if servers, err := Get("docker:servers"); err == nil && servers != nil {
-			return fmt.Errorf("Your scheduler is the segregate. Please remove the servers conf in docker.")
-		}
-		for _, value := range []string{"docker:scheduler:redis-server", "docker:scheduler:redis-prefix"} {
-			if _, err := Get(value); err != nil {
-				return fmt.Errorf("You should configure %s.", value)
-			}
-		}
-		return nil
-	}
-	if servers, err := Get("docker:servers"); err != nil || servers == nil {
-		return fmt.Errorf("You should configure the docker servers.")
-	}
-	return nil
-}
-
-// Check Router
-// It's verify your router configuration and validate related confs.
-func CheckRouter() error {
-	if router, err := Get("docker:router"); err == nil && router == "hipache" {
-		if hipache, err := Get("hipache"); err != nil || hipache == nil {
-			return fmt.Errorf("You should configure hipache router")
+func Check(checkers []Checker) error {
+	for _, check := range checkers {
+		err := check()
+		if err != nil {
+			return err
 		}
 	}
 	return nil
--- checker_test.go
+++ checker_test.go
@@ -5,6 +5,7 @@
 package config
 
 import (
+	"errors"
 	"launchpad.net/gocheck"
 )
 
@@ -12,119 +13,10 @@ type CheckerSuite struct{}
 
 var _ = gocheck.Suite(&CheckerSuite{})
 
-var configFixture = `
-listen: 0.0.0.0:8080
-host: http://127.0.0.1:8080
-debug: true
-admin-team: admin
-
-database:
-  url: 127.0.0.1:3435
-  name: tsuru
-
-git:
-  unit-repo: /home/application/current
-  api-server: http://127.0.0.1:8000
-  rw-host: 127.0.0.1
-  ro-host: 127.0.0.1
-
-auth:
-  user-registration: true
-  scheme: native
-
-provisioner: docker
-hipache:
-  domain: tsuru-sample.com
-queue: redis
-redis-queue:
-  host: localhost
-  port: 6379
-docker:
-  collection: docker_containers
-  repository-namespace: tsuru
-  router: hipache
-  deploy-cmd: /var/lib/tsuru/deploy
-  ssh-agent-port: 4545
-  segregate: true
-  scheduler:
-    redis-server: 127.0.0.1:6379
-    redis-prefix: docker-cluster
-  run-cmd:
-    bin: /var/lib/tsuru/start
-    port: 8888
-  ssh:
-    add-key-cmd: /var/lib/tsuru/add-key
-    public-key: /var/lib/tsuru/.ssh/id_rsa.pub
-    user: ubuntu
-`
-
-func (s *CheckerSuite) SetUpTest(c *gocheck.C) {
-	err := ReadConfigBytes([]byte(configFixture))
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TearDownTest(c *gocheck.C) {
-	configs = nil
-}
-
-func (s *CheckerSuite) TestCheckConfig(c *gocheck.C) {
-	err := Check()
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TestCheckDockerJustCheckIfProvisionerIsDocker(c *gocheck.C) {
-	configs["provisioner"] = "test"
-	err := CheckProvisioner()
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TestCheckDockerIsNotConfigured(c *gocheck.C) {
-	delete(configs, "docker")
-	err := CheckDocker()
+func (s *CheckerSuite) TestCheckExecuteCheckerFunc(c *gocheck.C) {
+	err := Check([]Checker{
+		func() error { return errors.New("Fake checker error") },
+	})
 	c.Assert(err, gocheck.NotNil)
-}
-
-func (s *CheckerSuite) TestCheckDockerBasicConfig(c *gocheck.C) {
-	err := CheckDockerBasicConfig()
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TestCheckDockerBasicConfigError(c *gocheck.C) {
-	delete(configs["docker"].(map[interface{}]interface{}), "collection")
-	err := CheckDockerBasicConfig()
-	c.Assert(err, gocheck.NotNil)
-}
-
-func (s *CheckerSuite) TestCheckSchedulerConfig(c *gocheck.C) {
-	err := CheckScheduler()
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TestCheckSchedulerSegregateWithServersConfig(c *gocheck.C) {
-	Set("docker:servers", []string{"server1", "server2"})
-	err := CheckScheduler()
-	c.Assert(err, gocheck.NotNil)
-}
-
-func (s *CheckerSuite) TestCheckSchedulerRoundRobinWithoutServersConfig(c *gocheck.C) {
-	Set("docker:segregate", false)
-	err := CheckScheduler()
-	c.Assert(err, gocheck.NotNil)
-}
-
-func (s *CheckerSuite) TestCheckSchedulerSegregateWithoutRedisConf(c *gocheck.C) {
-	Unset("docker:scheduler:redis-server")
-	err := CheckScheduler()
-	c.Assert(err, gocheck.NotNil)
-}
-
-func (s *CheckerSuite) TestCheckRouter(c *gocheck.C) {
-	err := CheckRouter()
-	c.Assert(err, gocheck.IsNil)
-}
-
-func (s *CheckerSuite) TestCheckRouterHipacheShouldHaveHipachConf(c *gocheck.C) {
-	Unset("hipache")
-	err := CheckRouter()
-	c.Assert(err, gocheck.NotNil)
+	c.Assert(err.Error(), gocheck.DeepEquals, "Fake checker error")
 }
