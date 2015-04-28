1fb3132fcee97c39e8894cb8aa8e90abdeaf98c7
Cezar Sa Espinola <cezarsa@gmail.com>
2015-04-28 13:45:01 -0300

    checker: allow warnings to be raised during check

--- checker.go
+++ checker.go
@@ -4,12 +4,36 @@
 
 package config
 
+import (
+	"fmt"
+	"io"
+)
+
 type Checker func() error
 
-// Check a parsed config file.
+type warningErr struct{ msg string }
+
+func (e *warningErr) Error() string {
+	return e.msg
+}
+
+func NewWarning(msg string) error {
+	return &warningErr{msg: msg}
+}
+
+// Check a parsed config file and consider warnings as errors.
 func Check(checkers []Checker) error {
+	return CheckWithWarnings(checkers, nil)
+}
+
+// Check a parsed config file and writes warnings to received writer.
+func CheckWithWarnings(checkers []Checker, warningWriter io.Writer) error {
 	for _, check := range checkers {
 		err := check()
+		if _, isWarn := err.(*warningErr); warningWriter != nil && isWarn {
+			fmt.Fprintf(warningWriter, "WARNING: %s\n", err)
+			continue
+		}
 		if err != nil {
 			return err
 		}
--- checker_test.go
+++ checker_test.go
@@ -5,6 +5,7 @@
 package config
 
 import (
+	"bytes"
 	"errors"
 
 	"gopkg.in/check.v1"
@@ -19,5 +20,22 @@ func (s *CheckerSuite) TestCheckExecuteCheckerFunc(c *check.C) {
 		func() error { return errors.New("Fake checker error") },
 	})
 	c.Assert(err, check.NotNil)
-	c.Assert(err.Error(), check.DeepEquals, "Fake checker error")
+	c.Assert(err.Error(), check.Equals, "Fake checker error")
+}
+
+func (s *CheckerSuite) TestCheckConsideringWarningsAsErrors(c *check.C) {
+	err := Check([]Checker{
+		func() error { return NewWarning("my warning") },
+	})
+	c.Assert(err, check.NotNil)
+	c.Assert(err.Error(), check.Equals, "my warning")
+}
+
+func (s *CheckerSuite) TestCheckWithWarnings(c *check.C) {
+	buf := bytes.NewBuffer(nil)
+	err := CheckWithWarnings([]Checker{
+		func() error { return NewWarning("my warning") },
+	}, buf)
+	c.Assert(err, check.IsNil)
+	c.Assert(buf.String(), check.Equals, "WARNING: my warning\n")
 }
